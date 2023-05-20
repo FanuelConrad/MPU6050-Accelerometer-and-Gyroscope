40e3287f6f8ced9b95be3d51f1129cda93904b71
Fanuel Conrad <59276995+FanuelConrad@users.noreply.github.com>
2023-05-20 22:20:48 +0300

    implemented void loop()

--- src/main.cpp
+++ src/main.cpp
@@ -31,3 +31,33 @@ void setup(void){
 
   delay(100);
 }
+
+void loop(){
+/*Get new sensor events with the readings*/
+sensors_event_t a, g, temp;
+mpu.getEvent(&a,&g,&temp);
+
+/*Print out the values*/
+Serial.print("Acceleration X: ");
+Serial.print(a.acceleration.x);
+Serial.print(", Y: ");
+Serial.print(a.acceleration.y);
+Serial.print(", Z: ");
+Serial.print(a.acceleration.z);
+Serial.println(" m/s^2");
+
+Serial.print("Rotation X");
+Serial.print(g.gyro.x);
+Serial.print(", Y: ");
+Serial.print(g.gyro.y);
+Serial.print(", Z: ");
+Serial.print(g.gyro.z);
+Serial.println(" rad/s");
+
+Serial.print("Temperature");
+Serial.print(temp.temperature);
+Serial.println(" degC");
+
+Serial.println("");
+delay(500);
+}
