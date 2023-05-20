47e544259951f5ac4d5d9f5a0d9792dafbf5fb2a
Fanuel Conrad <59276995+FanuelConrad@users.noreply.github.com>
2023-05-20 22:12:11 +0300

    implemented void setup()

--- src/main.cpp
+++ src/main.cpp
@@ -1,2 +1,33 @@
 #include <Arduino.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h> 
+#include <Wire.h>
 
+Adafruit_MPU6050 mpu;
+
+void setup(void){
+  Serial.begin(115200);
+
+  //Try to initialize
+  if (!mpu.begin())
+  {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1)
+    {
+      delay(10);
+    }
+    
+  }
+  Serial.println("MPU6050 found!");
+
+  //set accelerometer range to +-8G
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+
+  //set gyro range to +- 500 deg/s
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+
+  //set filer bandwidth to 21 Hz
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+
+  delay(100);
+}
