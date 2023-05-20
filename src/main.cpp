6c72b8b1dbf3142a7d8b4d4f4f750cdcf7bc8a16
Fanuel Conrad <59276995+FanuelConrad@users.noreply.github.com>
2023-05-20 23:03:51 +0300

    refactored void setup and void loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,13 @@
 #include <Arduino.h>
 #include <Adafruit_MPU6050.h>
-#include <Adafruit_Sensor.h> 
+#include <Adafruit_Sensor.h>
 #include <Wire.h>
 
 Adafruit_MPU6050 mpu;
 
-void setup(void){
-  Serial.begin(115200);
-
-  //Try to initialize
+void setupMPU6050()
+{
+  // Try to initialize
   if (!mpu.begin())
   {
     Serial.println("Failed to find MPU6050 chip");
@@ -16,48 +15,59 @@ void setup(void){
     {
       delay(10);
     }
-    
   }
   Serial.println("MPU6050 found!");
 
-  //set accelerometer range to +-8G
+  // set accelerometer range to +-8G
   mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
 
-  //set gyro range to +- 500 deg/s
+  // set gyro range to +- 500 deg/s
   mpu.setGyroRange(MPU6050_RANGE_500_DEG);
 
-  //set filer bandwidth to 21 Hz
+  // set filer bandwidth to 21 Hz
   mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
 
   delay(100);
 }
 
-void loop(){
-/*Get new sensor events with the readings*/
-sensors_event_t a, g, temp;
-mpu.getEvent(&a,&g,&temp);
+void readSensorValues()
+{
+  /*Get new sensor events with the readings*/
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
 
-/*Print out the values*/
-Serial.print("Acceleration X: ");
-Serial.print(a.acceleration.x);
-Serial.print(", Y: ");
-Serial.print(a.acceleration.y);
-Serial.print(", Z: ");
-Serial.print(a.acceleration.z);
-Serial.println(" m/s^2");
+  /*Print out the values*/
+  Serial.print("Acceleration X: ");
+  Serial.print(a.acceleration.x);
+  Serial.print(", Y: ");
+  Serial.print(a.acceleration.y);
+  Serial.print(", Z: ");
+  Serial.print(a.acceleration.z);
+  Serial.println(" m/s^2");
 
-Serial.print("Rotation X");
-Serial.print(g.gyro.x);
-Serial.print(", Y: ");
-Serial.print(g.gyro.y);
-Serial.print(", Z: ");
-Serial.print(g.gyro.z);
-Serial.println(" rad/s");
+  Serial.print("Rotation X");
+  Serial.print(g.gyro.x);
+  Serial.print(", Y: ");
+  Serial.print(g.gyro.y);
+  Serial.print(", Z: ");
+  Serial.print(g.gyro.z);
+  Serial.println(" rad/s");
 
-Serial.print("Temperature");
-Serial.print(temp.temperature);
-Serial.println(" degC");
+  Serial.print("Temperature");
+  Serial.print(temp.temperature);
+  Serial.println(" degC");
 
-Serial.println("");
-delay(500);
+  Serial.println("");
+}
+
+void setup(void)
+{
+  Serial.begin(115200);
+  setupMPU6050();
+}
+
+void loop()
+{
+  readSensorValues();
+  delay(500);
 }
