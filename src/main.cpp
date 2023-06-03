9b1363f717533de3f8035175769a067e639e7d90
Fanuel Conrad <59276995+FanuelConrad@users.noreply.github.com>
2023-06-03 21:32:05 +0300

    updated getAngles() and board definition

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,18 @@
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
 
+struct angle
+{
+  float x, y, z = 0;
+};
+
+angle position;
+
+unsigned long lastSampleMicros = 0;
+unsigned long lastPrintMillis = 0;
+
+#define INTERVAL_MS_PRINT 1000
+
 Adafruit_MPU6050 mpu;
 
 void setupMPU6050()
@@ -30,55 +42,75 @@ void setupMPU6050()
   delay(100);
 }
 
-//get roll angle in radians
-float getRollAngle(float ay, float az)
+angle calculateAccelerometerAngles()
 {
-  float roll = atan2f(ay, az); // radians
-  return roll;
-}
+  angle accelerometer;
 
-//get pitch angle in radians
-float getPitchAngle(float ax, float g){
-  float pitch=asinf((ax/g));
-  return pitch;
-}
-
-void readSensorValues()
-{
   /*Get new sensor events with the readings*/
   sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
+  accelerometer.x = (asinf(a.acceleration.x / 9.81));
+  accelerometer.y = (atan2f(a.acceleration.y, a.acceleration.z));
+  accelerometer.z = ((a.acceleration.z / 9.81));
 
-  /*Print out the values*/
-  Serial.print("Acceleration X: ");
-  Serial.print(a.acceleration.x);
-  Serial.print(", Y: ");
-  Serial.print(a.acceleration.y);
-  Serial.print(", Z: ");
-  Serial.print(a.acceleration.z);
-  Serial.println(" m/s^2");
+  return accelerometer;
+}
 
-  Serial.print("Rotation X: ");
-  Serial.print(g.gyro.x);
-  Serial.print(", Y: ");
-  Serial.print(g.gyro.y);
-  Serial.print(", Z: ");
-  Serial.print(g.gyro.z);
-  Serial.println(" rad/s");
+angle calculateGyroscopeAngles(unsigned long sampleMicros)
+{
+  angle gyroscope;
 
-  Serial.print("Temperature: ");
-  Serial.print(temp.temperature);
-  Serial.println(" degC");
+  /*Get new sensor events with the readings*/
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+  gyroscope.x = g.gyro.x * sampleMicros / 1000000;
+  gyroscope.y = g.gyro.y * sampleMicros / 1000000;
+  gyroscope.z = g.gyro.z * sampleMicros / 1000000;
 
-Serial.print("Pitch: ");
-  Serial.print(getPitchAngle(a.acceleration.x, 9.81));
-  Serial.println(" rad");
+  return gyroscope;
+}
 
-  Serial.print("Roll: ");
-  Serial.print(getRollAngle(a.acceleration.y, a.acceleration.z));
-  Serial.println(" rad");
+float getTemperature()
+{
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+  float temperature = temp.temperature;
 
-  Serial.println("");
+  return temperature;
+}
+
+void detectPitch(angle gyroscope, angle accelerometer)
+{
+  position.x = 0.98 * (position.x + degrees(gyroscope.x)) + 0.02 * degrees(accelerometer.x);
+}
+
+double getPitch()
+{
+  return position.x;
+}
+
+void detectRoll(angle gyroscope, angle accelerometer)
+{
+  position.y = 0.98 * (position.y + degrees(gyroscope.y)) + 0.02 * degrees(accelerometer.y);
+}
+
+double getRoll()
+{
+  return position.y;
+}
+
+bool readSample()
+{
+  unsigned long sampleMicros = (lastSampleMicros > 0) ? micros() - lastSampleMicros : 0;
+  lastSampleMicros = micros();
+
+  angle accelerometer = calculateAccelerometerAngles();
+  angle gyroscope = calculateGyroscopeAngles(sampleMicros);
+
+  detectPitch(gyroscope, accelerometer);
+  detectRoll(gyroscope, accelerometer);
+
+  return true;
 }
 
 void setup(void)
@@ -89,6 +121,30 @@ void setup(void)
 
 void loop()
 {
-  readSensorValues();
-  delay(500);
+  unsigned long currentMillis = millis();
+
+  readSample();
+
+  if (currentMillis - lastPrintMillis > INTERVAL_MS_PRINT)
+  {
+    Serial.print("TEMP:\t");
+    Serial.print(getTemperature());
+    Serial.print("\xC2\xB0"); // print degree symbol
+    Serial.print("C");
+    Serial.println();
+
+    Serial.print("Pitch:\t");
+    Serial.print(getPitch());
+    Serial.print("\xC2\xB0"); // print degree symbol
+    Serial.println();
+
+    Serial.print("Roll:\t");
+    Serial.print(getRoll());
+    Serial.print("\xC2\xB0"); // print degree symbol
+    Serial.println();
+
+    Serial.println();
+
+    lastPrintMillis = currentMillis;
+  }
 }
